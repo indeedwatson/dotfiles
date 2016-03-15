5ca2210416e200bd980f5b596d170e184b3c0e69
indeedwatson <beethovenopus111@gmail.com>
2016-03-15 12:49:37 -0300

    Delete livestreamer.sh

--- livestreamer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-## launch livestreamer with best as default quality
-channel=$1
-defquality=best
-quality=${2:-$defquality}
-
-livestreamer http://www.twitch.tv/$channel $quality --player=mpv &
