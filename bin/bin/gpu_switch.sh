98bc484d31540155892a44dc982aadcafd492a48
indeedwatson <beethovenopus111@gmail.com>
2017-08-24 17:30:20 -0300

    improve variable names

--- bin/bin/gpu_switch.sh
+++ bin/bin/gpu_switch.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-foo="/etc/mkinitcpio.conf"
-foo1="/etc/mkinitcpio.conf.radeon"
-foo2="/etc/mkinitcpio.conf.vfio"
+mkinit="/etc/mkinitcpio.conf"
+radeon="/etc/mkinitcpio.conf.radeon"
+vfio="/etc/mkinitcpio.conf.vfio"
 conf="/etc/X11/xorg.conf.d/"
 
 cd /etc/
 
-if diff "$foo" "$foo2" >/dev/null ;
+if diff "$mkinit" "$vfio" >/dev/null ;
 then
-    cp ""$foo1 "$foo"
-    echo "$foo1 is now active, backed up $foo2"
+    cp ""$radeon "$mkinit"
+    echo "$radeon is now active, backed up $vfio"
     mv $conf/10-intel.conf $conf/20-intel.conf
     mv $conf/20-radeon.conf $conf/10-radeon.conf
 
-elif diff "$foo" "$foo1" >/dev/null ;
+elif diff "$mkinit" "$radeon" >/dev/null ;
 then
-    cp "$foo2" "$foo"
-    echo "$foo2 is now active, backed up $foo1" 
+    cp "$vfio" "$mkinit"
+    echo "$vfio is now active, backed up $radeon" 
     mv $conf/20-intel.conf $conf/10-intel.conf
     mv $conf/10-radeon.conf $conf/20-radeon.conf
 else
-    echo "There's no foo"
+    echo "There's no mkinit"
 fi
 
 sudo mkinitcpio -p linux
