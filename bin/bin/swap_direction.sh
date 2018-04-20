3c6760f0da35ae227ea58f71b6f62f63612935a7
indeedwatson <beethovenopus111@gmail.com>
2018-04-20 01:16:57 -0300

    add visual, directional swap keybinds

---
+++ bin/bin/swap_direction.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# credit: nejni-marji on #i3
+# swap direction: swap two containers based on their direction, visually
+# depends on https://github.com/miseran/i3-tools/blob/master/focus-tool
+
+DIRECTION=$1
+i3-msg -- mark --add A
+~/i3-tools/focus-tool $DIRECTION
+i3-msg -- mark add B
+i3-msg swap container with mark A
+i3-msg '[con_mark="A"] focus'
+i3-msg '[con_mark="A"] unmark A'
+i3-msg '[con_mark="B"] unmark B'
